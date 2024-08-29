d1aea00990ac6f0fba1ee3407c487de21129766d
webstafrank <franklyndenish1@gmail.com>
2024-08-29 06:43:21 -0700

    App runner

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-# run.py
-from app import create_app, db
-from flask_migrate import Migrate
+from app import create_app
 
 app = create_app()
-migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run(debug=True)
 
