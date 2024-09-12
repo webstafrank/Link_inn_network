ae3d9a39fb553b757c3abc49e08388789f75c44b
webstafrank <franklyndenish1@gmail.com>
2024-09-12 02:56:57 -0700

    Route for packages and payments

--- app/routes/api_routes.py
+++ app/routes/api_routes.py
@@ -1,25 +1,10 @@
-# app/routes/api_routes.py
-from flask import Blueprint, jsonify, request
-from app.models import Package, BlogPost, Transaction, db
+from flask import jsonify, request
+from app.models.package import Package
 
-api = Blueprint('api', __name__)
-
-@api.route('/api/packages', methods=['GET'])
+@app.route('/api/packages', methods=['GET'])
 def get_packages():
     packages = Package.query.all()
-    return jsonify([{"id": p.id, "name": p.name, "speed": p.speed, "price": p.price} for p in packages])
+    packages_data = [{"id": p.id, "name": p.name, "price": p.price} for p in packages]
+    return jsonify(packages_data)
 
-@api.route('/api/purchase', methods=['POST'])
-def purchase():
-    data = request.get_json()
-    # Process the purchase logic here (including payment via Stripe)
-    transaction = Transaction(user_id=data['user_id'], package_id=data['package_id'], amount=data['amount'], status='Completed')
-    db.session.add(transaction)
-    db.session.commit()
-    return jsonify({"message": "Transaction successful."})
-
-@api.route('/api/blogs', methods=['GET'])
-def get_blogs():
-    posts = BlogPost.query.all()
-    return jsonify([{"id": p.id, "title": p.title, "content": p.content, "timestamp": p.timestamp} for p in posts])
 
