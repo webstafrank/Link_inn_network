b1c8657b876d6f2ed069698b40955c6b16257a37
webstafrank <franklyndenish1@gmail.com>
2024-09-12 03:46:16 -0700

    Post Model

---
+++ app/models/post.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(100), nullable=True)
+    comments = db.relationship('Comment', backref='post', lazy=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
