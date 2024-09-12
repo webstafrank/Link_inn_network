afdb057112bf2e3be107ecfb7975e4e87ca8ffd9
webstafrank <franklyndenish1@gmail.com>
2024-09-12 06:06:00 -0700

    Configure the app

--- config.py
+++ config.py
@@ -1,9 +1,23 @@
-# config.py
 import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://username:password@localhost/linkinn'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STRIPE_API_KEY = os.environ.get('STRIPE_API_KEY') or 'your_stripe_api_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+
+    # Quikk.dev API credentials
+    QUIKK_DEV_API_KEY = os.environ.get('QUIKK_DEV_API_KEY')
+    QUIKK_DEV_API_SECRET = os.environ.get('QUIKK_DEV_API_SECRET')
+
+    QUIKK_PROD_API_KEY = os.environ.get('QUIKK_PROD_API_KEY')
+    QUIKK_PROD_API_SECRET = os.environ.get('QUIKK_PROD_API_SECRET')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    QUIKK_API_KEY = Config.QUIKK_DEV_API_KEY
+    QUIKK_API_SECRET = Config.QUIKK_DEV_API_SECRET
+
+class ProductionConfig(Config):
+    DEBUG = False
+    QUIKK_API_KEY = Config.QUIKK_PROD_API_KEY
+    QUIKK_API_SECRET = Config.QUIKK_PROD_API_SECRET
 
